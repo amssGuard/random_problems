da4a0e08e6e0573c6e7b5f50b9ba26c432206996
anoshkekal <97592173+anoshkekal@users.noreply.github.com>
2022-11-30 17:15:04 +0530

    Printing sequence of number with out using loop.
    Print a pattern without using any loop (using recursion):
    a[0]=n a[i+1]=a[i]-1 until a[i]>0 . after that a[i+1]=a[i]+5 until a[i]=N

---
+++ recur.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class recur{
+	public static int recure(int n) {
+	if(n>0) {
+		System.out.print("\t"+n);
+		recure(n-5);
+		System.out.print("\t"+n);
+	} else {
+		System.out.print("\t"+n);
+	}
+	return 0;
+	}
+	public static void main(String[]args) {
+		int n=16;
+		recure(n);
+		
+	}
+}
