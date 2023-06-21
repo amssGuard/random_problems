9fa6882b86b2a93a03eff099fca7fddc3bb3e174
Anoshkekal <97592173+anoshkekal@users.noreply.github.com>
2023-06-21 22:37:02 +0530

    Add files via upload
    setting a game object to trigger an event to change the level

---
+++ LevelTrigger.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+   
+}
